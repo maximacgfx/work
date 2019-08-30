f119ca1925dd5aab26af6c0e75864afa8a06d1f9
maximacgfx <cgfxmaxima@yandex.ru>
2019-08-30 11:13:16 +0400

    task_4_5.py Done

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -13,3 +13,7 @@
 
 command1 = 'switchport trunk allowed vlan 1,2,3,5,8'
 command2 = 'switchport trunk allowed vlan 1,3,8,9'
+
+print('\n',command1,'\n',command2,'\n')
+
+print (sorted (set((command1.split())[-1].split(',')) & set((command2.split())[-1].split(','))))
